7f37fdf8d088e9e6204bfa3b7a67fe155c40ff4c
lopera.william@gmail.com <William>
2022-07-07 15:00:49 -0500

    publicar en netlify con llamada a nodejs remoto

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,3 +1,4 @@
 window["runConfig"] = {
-  BACKEND_URL: "http://localhost:8585/api",
+  BACKEND_URL: "http://localhost:8585/api", //Desarrollo
+  // BACKEND_URL: "https://node-bakery-formula.herokuapp.com/api", // Produccion
 };
