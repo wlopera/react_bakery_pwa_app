7af31fc4d0eb1cbf1b4e5029fc47ebdd56e91193
William Lopera <con_wlopera@credicorpbank.com>
2022-12-27 14:48:37 -0500

    deploy en vecel

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,4 +1,5 @@
 window["runConfig"] = {
-  BACKEND_URL: "http://localhost:8585/api", //Desarrollo
-  // BACKEND_URL: "https://node-bakery-formula.herokuapp.com/api", // Produccion
+  // BACKEND_URL: "http://localhost:8585/api", //Desarrollo
+  // BACKEND_URL: "https://node-bakery-formula.herokuapp.com/api", // Produccion Heroku
+  BACKEND_URL: "https://vercel.com/wlopera/node-bakery-formula/api", // Produccion Vecel
 };
