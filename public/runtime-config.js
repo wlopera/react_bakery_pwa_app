9b8a20f6feefad9f5ae1fe44eb52f98e2f7f4c29
William Lopera <lopera.william@gmail.com>
2022-12-28 15:03:03 -0500

    asjuet por reversoa mongodb

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,5 +1,5 @@
 window["runConfig"] = {
-  // BACKEND_URL: "http://localhost:8585/api", //Desarrollo
+  // BACKEND_URL: "http://localhost:8585/api",                     //Desarrollo
   // BACKEND_URL: "https://node-bakery-formula.herokuapp.com/api", // Produccion Heroku
   BACKEND_URL: "https://node-bakery-formula.vercel.app/api", // Produccion Vercel
 };
